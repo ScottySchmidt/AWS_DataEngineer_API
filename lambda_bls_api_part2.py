e89f86f0a8d865b53c197abb7d6fab0ae374ec07
Scott Schmidt, MBA <scott.schmidt1989@yahoo.com>
2025-08-08 14:19:44 -0500

    Create lambda_bls_api_part2.py

---
+++ lambda_bls_api_part2.py
@@ -0,0 +1,25 @@
+#THIS IS MY LAMBDA FUNCTION WITHIN AWS:
+import os, json, boto3
+from datetime import datetime
+import urllib.request
+import os
+
+s3 = boto3.client("s3", region_name="us-east-1")
+
+def lambda_handler(event, context):
+    bucket = os.environ.get("BUCKET_NAME")
+    url = "https://honolulu-api.datausa.io/tesseract/data.jsonrecords?cube=acs_yg_total_population_1&drilldowns=Year%2CNation&locale=en&measures=Population"
+
+    try:
+        with urllib.request.urlopen(url) as response:
+            data = response.read()
+            response_json = json.loads(data)
+    except Exception as e:
+        return {"statusCode": 500, "body": f"API request failed: {e}"}
+    key = "datausa_population.json"
+    #key = f"datausa_population_{datetime.now().strftime('%Y%m%d%H%M%S')}.json"
+    try:
+        s3.put_object(Bucket=bucket, Key=key, Body=json.dumps(response_json))
+    except Exception as e:
+        return {"statusCode": 500, "body": f"S3 upload failed: {e}"}
+    return {"statusCode": 200, "body": f"✅ Uploaded key to bucket"}
